536b4d9806ec94144bf3fbc1f1169ee7de077f78
Saif Al-Diresee <saif.aldiresee@gmail.com>
2025-09-11 12:20:43 +0200

    update main.ts

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -3,7 +3,8 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors();
-  await app.listen(process.env.PORT ?? 5000);
+  app.setGlobalPrefix('api');
+  await app.listen(process.env.PORT || 5000);
 }
 bootstrap();
+
